0dd461359f12d6e5308fc2f2b5513188e7849116
ShravaniMishra <shravniguddi@gmail.com>
2023-03-26 21:55:31 +0530

    Infinite Scrolling Implemented

--- Infinite Scrolling/src/Components/Home.jsx
+++ Infinite Scrolling/src/Components/Home.jsx
@@ -1,16 +1,40 @@
-import React, { useEffect, useId, useState } from "react";
-import './Home.css'
+import React, { useEffect, useState } from "react";
+import "./Home.css";
 
 const Home = () => {
-  const [data, setData] = useState("");
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   const getData = async () => {
-    let result = await fetch("https://randomuser.me/api?results=500");
-    let data = await result.json();
-    console.log(data);
-    setData(data.results);
+    setLoading(true);
+    try {
+      let result = await fetch("https://randomuser.me/api?results=20");
+      let responseData = await result.json();
+      responseData = responseData.results;
+      console.log(responseData.length);
+      setTimeout(() => {
+        setData((prev) => [...prev, ...responseData]);
+        setLoading(false);
+      }, 1000);
+    } catch (error) {
+      console.log("Error while getting data", error);
+    }
   };
+  const handleScroll = () => {
+    let innerHeight = window.innerHeight;
+    let totalHeight = document.documentElement.scrollHeight;
+    let scrollTop = document.documentElement.scrollTop;
+    // console.log(
+    //   `innerHeight ${innerHeight} scrollTop ${scrollTop} totalHeight ${totalHeight}`
+    // );
+    if (innerHeight + scrollTop + 1 >= totalHeight) {
+      getData();
+    }
+  };
+
   useEffect(() => {
     getData();
+    window.addEventListener("scroll", handleScroll);
   }, []);
   return (
     <>
@@ -18,15 +42,16 @@ const Home = () => {
         {data &&
           data.map((elem, ind) => {
             return (
-              <div key={ind}>
+              <div key={elem.login.uuid}>
                 <h1>
-                  {elem.name.first }
-                  { elem.name.last}
+                  {elem.name.first}
+                  {elem.name.last}
                 </h1>
                 <img src={elem.picture.thumbnail} alt="thumb" />
               </div>
             );
           })}
+        {loading && <h1>Loading...</h1>}
       </div>
     </>
   );
